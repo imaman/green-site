a57aaec1d7650b7880e878fabedfaa0fd0b551d9
imaman <imaman@google.com>
2014-02-03 17:57:12 +0200

    RED: assertons do not throw

--- spec/explore.spec.js
+++ spec/explore.spec.js
@@ -2,5 +2,16 @@ describe('jasmine-node failure semantics', function() {
   it('times out if done() is not called', function(done) {
     done();
   });
+
+  it('assertion failures are not reported via exceptions', function(done) {
+    var captured = null;
+    try {
+      expect(990099).toEqual(3);
+    } catch(e) {
+      captured = e;
+    }
+    expect(captured).toBe(null);
+    done();
+  });
 });
 
