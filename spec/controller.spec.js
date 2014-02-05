e6ffe6b4119fdb7022388d37f17754ec79dc9f50
imaman <imaman@google.com>
2014-02-05 14:30:10 +0200

    RED: failing test for new functionality (reverse chronological ordering)

--- spec/controller.spec.js
+++ spec/controller.spec.js
@@ -16,5 +16,25 @@ describe('controller', function() {
       expect(view).toEqual('posts');
       expect(data.posts.map(function(x) { return x.body })).toEqual(['b_1', 'b_2']);
     });
+
+    it('shows the most recent post first', function() {
+      var monday = '2014-02-03T14:00:00Z';
+      var tuesday = '2014-02-04T10:00:00Z';
+      var posts = [ 
+        {id: 1, publishedAt: monday, body: 'Post from Monday'},
+        {id: 2, publishedAt: tuesday, body: 'Post from Tuesday'}
+      ];
+      var controller = controllerModule.withModel({ posts: posts });
+
+      var view = null;
+      var data = null;
+      controller.posts(null, { render: function(v, d) {
+        view = v;
+        data = d
+      }});
+
+      expect(data.posts.map(function(x) { return x.body })).toEqual(
+        ['Post from Tuesday', 'Post from Monday']);
+    });
   });
 });
