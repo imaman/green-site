ccce14df13c60598ef25573b3a7db3f0ea5192df
imaman <imaman@google.com>
2014-02-05 17:19:35 +0200

    additional tests for views/post

--- spec/views.spec.js
+++ spec/views.spec.js
@@ -17,4 +17,26 @@ describe('post view', function() {
     expect(html).toContain('Title');
     expect(html).toContain('Body');
   });
+
+  it('by default, it does not show the widgets', function() {
+    var html = jade.renderFile(pathOf('post.jade'), { 
+      options: {},
+      post: {}
+    });
+    expect(html).not.toContain('addthis_config');
+  });
+  it('it does not show the widgets when the showWidgets option is false', function() {
+    var html = jade.renderFile(pathOf('post.jade'), { 
+      options: { showWidgets: false },
+      post: {}
+    });
+    expect(html).not.toContain('addthis_config');
+  });
+  it('it does show the widgets when the showWidgets option is true', function() {
+    var html = jade.renderFile(pathOf('post.jade'), { 
+      options: { showWidgets: true },
+      post: {}
+    });
+    expect(html).toContain('addthis_config');
+  });
 });
