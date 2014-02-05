37ba87655b7353b70904180142672f3b2d1aa038
imaman <imaman@google.com>
2014-02-05 13:52:56 +0200

    GREEN: create the necessary scaffolding

--- spec/controller.spec.js
+++ spec/controller.spec.js
@@ -4,7 +4,13 @@ describe('controller', function() {
   describe('posts', function() {
     it('returns all posts', function() {
       var controller = controllerModule.withModel({ posts: [ {id: 1}, {id: 2}, {id: 3} ] });
-      controller.posts();
+
+      var view = null;
+      var data = null;
+      controller.posts(null, { render: function(v, d) {
+        view = v;
+        data = d
+      }});
     });
   });
 });
