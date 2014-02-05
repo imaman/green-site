1b124e0ce9740bf094fa9390aa588eff1b4aa6e4
imaman <imaman@google.com>
2014-02-05 16:20:23 +0200

    GREEN

--- controller.js
+++ controller.js
@@ -3,7 +3,9 @@ var moment = require('moment');
 exports.withModel = function(model) {
   var controller = {};
   controller.posts = function(req, res) {
-    var sorted = model.posts.slice(0);
+    var sorted = model.posts.filter(function(x) {
+      return !x.marks || x.marks.indexOf('main') >= 0;
+    });
     sorted.sort(function(lhs, rhs) {
       return new Date(rhs.publishedAt).getTime() - new Date(lhs.publishedAt).getTime();
     });
