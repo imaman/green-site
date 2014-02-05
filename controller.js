9a5481d6c701ab6870fad963cbf56496f40868ac
imaman <imaman@google.com>
2014-02-05 13:37:13 +0200

    refactoring: pull the method in

--- controller.js
+++ controller.js
@@ -4,16 +4,15 @@ var model = {};
 
 exports.withModel = function(model_) {
   model = model_;
+  exports.posts = function(req, res) {
+    var posts = model.posts.map(function(post) {
+      var result = Object.create(post);
+      result.publishedAt = moment(result.publishedAt).fromNow();
+      return result;
+    });
+    res.render('posts', { posts: posts, headline: model.headline });
+  }
+
   return exports;
 }
-
-exports.posts = function(req, res) {
-  var posts = model.posts.map(function(post) {
-    var result = Object.create(post);
-    result.publishedAt = moment(result.publishedAt).fromNow();
-    return result;
-  });
-  res.render('posts', { posts: posts, headline: model.headline });
-}
-
 
