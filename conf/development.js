bd251994b7c862760ab8f680eaf53681e5928424
imaman <imaman@google.com>
2014-02-12 10:19:04 +0200

    add mongodb support

--- conf/development.js
+++ conf/development.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: 3000,
-  GOOGLE_HOSTNAME: 'http://localhost:3000'
-
+  GOOGLE_HOSTNAME: 'http://localhost:3000',
+  MONGOHQ_URL: 'mongodb://localhost/collidingobjects-dev'
 };
