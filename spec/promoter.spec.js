51c1aae747142c821198404ca60c0a6a1c6f4644
imaman <itay.maman@gmail.com>
2014-02-17 00:56:45 +0200

    RED: the stubbying game -- TypeError: Object #<DeployerStub> has no method 'deploy'

--- spec/promoter.spec.js
+++ spec/promoter.spec.js
@@ -15,7 +15,7 @@ promoter.__set__('acceptanceSpecs', {});
 function DeployerStub() {
   this.init = function(done) { done(); };
   this.mostRecentRelease = function(app, done) { done(null, { slug: { id: app + '_slug_id' }}); };
-  this.fetchReleases = function(app, done) { done(null, []); };
+  this.fetchReleases = function(app, done) { done(null, [ { slug: { id: app + '_slug_id' }} ]); };
 }
 
 promoter.__set__('Deployer', DeployerStub);
