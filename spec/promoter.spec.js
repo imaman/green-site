41450c0c459975add6c84505e718f24652188866
imaman <itay.maman@gmail.com>
2014-02-17 00:31:51 +0200

    RED: the stubbying game -- TypeError: object is not a function

--- spec/promoter.spec.js
+++ spec/promoter.spec.js
@@ -2,13 +2,14 @@ var rewire = require('rewire');
 var promoter = rewire('../acceptance/promoter.js');
 
 promoter.__set__('JasmineNodeApi', {});
-promoter.__set__('acceptanceSpecs', {})
-promoter.__set__('Promoter', {});
+promoter.__set__('acceptanceSpecs', {});
+promoter.__set__('Deployer', {});
 
 
 
 describe('promoter', function() {
   it('does something', function(done) {
+    promoter('a', 'b');
     done();
   });
 });
